587883da53d0747c5bd5de88bcef3c79a1b1788e
Oleg <loytloloyu@gmail.com>
2021-02-17 23:08:31 +0200

    к

--- src/main/java/org/example/designpatterns/designpatterns/createservice/fix/createapiservice/ApiСontactServiceFactoryсlass.java
+++ src/main/java/org/example/designpatterns/designpatterns/createservice/fix/createapiservice/ApiСontactServiceFactoryсlass.java
@@ -1,14 +1,20 @@
 package org.example.designpatterns.designpatterns.createservice.fix.createapiservice;
 
+import lombok.Data;
 import org.example.configuration.ConfigLoader;
+import org.example.services.UsersService;
 
+import java.util.Map;
+
+@Data
 public class ApiСontactServiceFactoryсlass implements ApiСontactServiceFactory {
 
     ConfigLoader configLoader;
+    UsersService usersService;
+    Map<String, Object> tools;
 
     @Override
     public ApiСontactServiceFactory createService() {
-
 
         return null;
     }
---
+++ src/main/java/org/example/entity/Tools.java
@@ -0,0 +1,16 @@
+package org.example.entity;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Tools {
+
+
+    Map <String, Object>tools = new HashMap<>();
+
+
+
+
+
+
+}
