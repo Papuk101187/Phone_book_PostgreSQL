8633294ff316bd5a440de7c379135f0b6b7c4392
Oleg <loytloloyu@gmail.com>
2021-02-18 10:35:57 +0200

    fisk part 3

--- src/main/java/org/example/designpatterns/designpatterns/createservice/fix/creatememorycontactservice/InMemoryContactServiceFactory.java
+++ src/main/java/org/example/designpatterns/designpatterns/createservice/fix/creatememorycontactservice/InMemoryContactServiceFactory.java
@@ -1,8 +1,9 @@
 package org.example.designpatterns.designpatterns.createservice.fix.creatememorycontactservice;
 
 import org.example.designpatterns.designpatterns.createservice.fix.createapiservice.ApiСontactServiceFactory;
+import org.example.services.ContactService;
 
 public interface InMemoryContactServiceFactory {
 
-    ApiСontactServiceFactory createService();
+    ContactService createService();
 }
--- src/main/java/org/example/designpatterns/designpatterns/createservice/fix/creatememorycontactservice/InMemoryContactServiceFactoryclass.java
+++ src/main/java/org/example/designpatterns/designpatterns/createservice/fix/creatememorycontactservice/InMemoryContactServiceFactoryclass.java
@@ -1,10 +1,20 @@
 package org.example.designpatterns.designpatterns.createservice.fix.creatememorycontactservice;
 
+import org.example.configuration.ApplicationGetPropertys;
 import org.example.designpatterns.designpatterns.createservice.fix.createapiservice.ApiСontactServiceFactory;
+import org.example.services.ContactService;
+import org.example.services.implementation.contact.InMemoryContactService;
 
 public class InMemoryContactServiceFactoryclass implements InMemoryContactServiceFactory{
+
+    ApplicationGetPropertys applicationGetPropertys;
+
+    public InMemoryContactServiceFactoryclass(ApplicationGetPropertys applicationGetPropertys) {
+        this.applicationGetPropertys = applicationGetPropertys;
+    }
+
     @Override
-    public ApiСontactServiceFactory createService() {
-        return null;
+    public ContactService createService() {
+        return new InMemoryContactService();
     }
 }
