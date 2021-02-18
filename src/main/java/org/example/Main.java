955292399575944394567159c5956e4b81499c10
Oleg <loytloloyu@gmail.com>
2021-02-18 10:13:27 +0200

    correcting error in factory

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,13 +1,10 @@
 package org.example;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.example.configuration.ApplicationGetPropertys;
 import org.example.configuration.ConfigLoader;
 import org.example.configuration.CreateService;
 import org.example.designpatterns.designpatterns.createservice.fix.createapiservice.ApiСontactServiceFactory;
 import org.example.designpatterns.designpatterns.createservice.fix.createapiservice.ApiСontactServiceFactoryсlass;
-import org.example.entity.BulderTools;
-import org.example.entity.Tools;
 import org.example.entity.User;
 import org.example.menu.PhoneBook;
 import org.example.services.ContactService;
@@ -15,7 +12,6 @@ import org.example.services.UsersService;
 import org.example.services.implementation.contact.NioContactService;
 
 import java.io.IOException;
-import java.net.http.HttpClient;
 
 public class Main {
 
@@ -23,27 +19,14 @@ public class Main {
 
         ConfigLoader configLoader = new ConfigLoader(); // загружает properties из файла
 
-
-        Tools tools = BulderTools.bulderTools // наполняем необходимыми инструментами
-                .addToolsforHttpClient(HttpClient.newBuilder().build())
-                .addToolsforObjectMapper(new ObjectMapper())
-                .addToolsforConfig(configLoader).build();
-        tools.packing(); // упаковуем наши инструменты для передачи в фабрику
-
-
-        ApiСontactServiceFactory apiСontactServiceFactory =
-                new ApiСontactServiceFactoryсlass(tools.getTools());
-
-
-
-
-
-
-
         String profile = configLoader.getProfile(); // получаем профиль из системы
-        String configFile = "app-"+profile+".properties"; // получаем имя файла
+        String configFile = "app-" + profile + ".properties"; // получаем имя файла
 
         ApplicationGetPropertys properties = configLoader.load(configFile); // единый класс с properties
+
+        ApiСontactServiceFactory apiСontactServiceFactoryсlass
+                = new ApiСontactServiceFactoryсlass(properties);
+
 
         CreateService createService = new CreateService(properties); // создаём сервис по нашим properties
         createService.BuildService(); // строим сервис
@@ -55,8 +38,6 @@ public class Main {
 
         PhoneBook phoneBook = new PhoneBook(contactService1, usersService, new User()); // наша телефонная книга
         phoneBook.start();
-
-
 
 
     }
--- src/main/java/org/example/designpatterns/designpatterns/createservice/fix/createapiservice/ApiСontactServiceFactoryсlass.java
+++ src/main/java/org/example/designpatterns/designpatterns/createservice/fix/createapiservice/ApiСontactServiceFactoryсlass.java
@@ -1,7 +1,9 @@
 package org.example.designpatterns.designpatterns.createservice.fix.createapiservice;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import org.example.configuration.ApplicationGetPropertys;
 import org.example.configuration.ConfigLoader;
 import org.example.services.ContactService;
 import org.example.services.UsersService;
@@ -11,28 +13,31 @@ import org.example.services.implementation.user.ApiUserService;
 import java.net.http.HttpClient;
 import java.util.Map;
 
-
 public class ApiСontactServiceFactoryсlass implements ApiСontactServiceFactory {
-    Map<String, Object> tools;
 
-    public ApiСontactServiceFactoryсlass(Map<String, Object> tools) {
-        this.tools = tools;
+    public ApiСontactServiceFactoryсlass(ApplicationGetPropertys applicationGetPropertys) {
+        this.applicationGetPropertys = applicationGetPropertys;
     }
 
-    HttpClient httpClient = (HttpClient) tools.get("HttpClient");
-    ObjectMapper objectMapper = (ObjectMapper) tools.get("ObjectMapper");
-    ConfigLoader configLoader = (ConfigLoader) tools.get("ConfigLoader");
+    ApplicationGetPropertys applicationGetPropertys;
 
+    ObjectMapper objectMapper = new ObjectMapper();
+    HttpClient httpClient = HttpClient.newBuilder().build();
 
-    UsersService usersService;
-    ContactService contactService;
 
     @Override
     public ContactService createService() {
-        usersService = new ApiUserService();
-        contactService = new ApiContactService()
-        return contactService;
-    }
 
+        UsersService usersService = new ApiUserService(
+                applicationGetPropertys.getBaseURLregistration(),
+                applicationGetPropertys.getBaseURLlogin(), objectMapper, httpClient);
+        return new ApiContactService(
+                usersService,
+                objectMapper, httpClient,
+                applicationGetPropertys.getBaseURLadd(),
+                applicationGetPropertys.getBaseURLsearch(),
+                applicationGetPropertys.getBaseURLadd());
+
+    }
 
 }
